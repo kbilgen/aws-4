c140a3519cb64d7fe23a0415dcc792060c4f5ef2
Kaan Bilgen <kbilgen1980@gmal.com>
2024-02-05 14:48:43 +0300

    Yerel değişiklikleri commit et

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "aws4dcf55e2b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
